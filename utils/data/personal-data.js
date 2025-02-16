5bd67bfc04d9dcc9713d205d9ef1c09bef9f27db
AkshayGojiya <152589405+AkshayGojiya@users.noreply.github.com>
2025-02-16 22:24:34 +0530

    change in email address

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/profile.jpg',
   designation: "Software Developer",
   description: "My name is Akshay. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'aksgojiya.com',
+  email: 'aksgojiya@gmail.com',
   phone: '+91 9510469749',
   address: 'Anand Gujrat',
   github: 'https://github.com/AkshayGojiya',
